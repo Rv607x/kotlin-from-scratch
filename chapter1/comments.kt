4c5360ade876497858adab3da9b9dffe43ce8452
Rv607x <19752692+Rv607x@users.noreply.github.com>
2025-12-16 21:08:21 +0300

    Added single line, multiple lines and documentation comments

--- chapter1/comments.kt
+++ chapter1/comments.kt
@@ -1,3 +1,17 @@
 fun main() {
     println("Hello world")
+    // single line comments
+
+    /* 
+    Multiple line 
+    commments. Just like Java 
+    */
+
+    /** 
+    * this is a documentation comments
+    *
+    *@param none given
+    *@return none returned
+    */
+    println("Just learning about comments in Kotlin")
 }
