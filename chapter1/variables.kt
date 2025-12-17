c45b35c7953d13f00186af36af5a036581b23a3a
Rv607x <19752692+Rv607x@users.noreply.github.com>
2025-12-17 12:12:48 +0300

    added inferring of variables

--- chapter1/variables.kt
+++ chapter1/variables.kt
@@ -12,10 +12,22 @@ val age = 20
 fun main() {
     println("The name is $name and they are $age years old")
     trial()
+    println("----- Type infering--------")
+    trial2()
 }
 
 fun trial() {
     var name = "Cheryl"
     val age = 34
     println("The name is $name and they are $age years old")
+}
+
+//infering of variable type 
+
+fun trial2() {
+    val name: String = "Johnte"
+    val pi: Double = 3.142
+    val dob: Int = 31
+    println("Name is $name and they were born on $dob")
+    println("The value of pi is $pi")
 }
