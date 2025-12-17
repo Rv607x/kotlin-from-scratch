0721a4e2dac6e0980d764ab78dbdae742a75d4f8
Rv607x <19752692+Rv607x@users.noreply.github.com>
2025-12-17 14:17:34 +0300

    Added relational operators and logical operators

--- chapter1/operators.kt
+++ chapter1/operators.kt
@@ -27,15 +27,34 @@ fun main(){
     s += "te"
     println(s)
     
+    
     println("")
     println("<------Unary Operators------->")
     /* unary operator increment (++) and decrement (--) unary operators increase or 
     decrease a variable’s value by 1
     */
-    
     var x = 10
     x++
     println(x)
     x--
     println(x)
+
+    println("")
+    println("<------relational Operators------->")
+    val isEqual = (a == c)
+    println(isEqual)
+    var isNotEqual = (a != x)
+    println(isNotEqual)
+    var isGreater = (x > a)
+    println(isGreater)
+
+    println("")
+    println("<------Logical Operators------->")
+    /* Logical operators are used to perform logical operations such as AND (&&),
+    OR (||), and NOT (!) on Boolean values. */
+    val z = true
+    val y = false
+    println(z && y)
+    println(z || y)
+    println( !z)
 }
